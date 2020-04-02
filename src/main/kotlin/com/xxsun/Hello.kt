2ef0e17fe6134941c8c93a529d6d640dd6a7dc58
SunKenneth <xx.sun@msa.hinet.net>
2020-04-02 22:52:44 +0800

    test bmi

--- src/main/kotlin/com/xxsun/Hello.kt
+++ src/main/kotlin/com/xxsun/Hello.kt
@@ -1,5 +1,17 @@
 package com.xxsun
 
 fun main(){
-    println("Hello kotlin")
+//    println("Hello kotlin")
+    val p=Human()
+    p.hello()
+    p.bmi()
+}
+
+class  Human(){
+    fun hello(){
+        println("Hello Kitlin")
+    }
+    fun bmi(){
+        println("BMI=")
+    }
 }
